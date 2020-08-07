f53cee30e517a7df780ca5a7f34fa8c8153cb55d
SakiSugio <67557479+SakiSugio@users.noreply.github.com>
2020-08-06 21:56:20 -0500

    Update main.py

---
+++ python-challenge/PyBank/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+
+# Files to load and output 
+file_to_load = os.path.join("PyBank/Resources", "budget_data.csv")
+#file_to_output = os.path.join("analysis", "paragraph_analysis.txt")
